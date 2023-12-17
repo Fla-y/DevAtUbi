9323bb6714a286e7b7a475e115833e51b1bec846
Fla <Fla@LAPTOP-5LU27L97>
2023-12-17 21:15:17 +0100

    removed some files to restart

--- GameTest/LevelBuilder.h
+++ GameTest/LevelBuilder.h
@@ -3,22 +3,5 @@
 
 #include "stdafx.h"
 
-class LevelBuilder {
-public:
-    // Constructor takes the vector from FileReader
-    LevelBuilder(const std::vector<char>& fileContents);
-
-    void BuildLevel(const std::vector<char>& fileContents, const std::vector<std::vector<int>>& levelData);
-
-    const std::vector<std::vector<int>>& GetLevel() const;
-
-    bool ShouldLoadNextLevel() const;
-
-private:
-
-    std::vector<std::vector<int>> level;
-    bool loadNextLevel;
-};
-
 #endif 
 
--- GameTest/Physics.h
+++ GameTest/Physics.h
@@ -1,13 +1,6 @@
 #ifndef PHYSICS_H
 #define PHYSICS_H
 
-#include "stdafx.h"
-#include "app/app.h"
-
-class Physics {
-public:
-    static void ApplyGravity(float& positionY, float& velocityY, float deltaTime, float gravity);
-};
 
 #endif 
 
